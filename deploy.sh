9ed2548543c02c0ec73a6f1c404d88e0afd38bf7
Eduard Wayland <eduproard@gmail.com>
2022-03-10 13:56:50 +0100

    First commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [[ "$1" =~ ^$ ]]; then
+    echo "You must specify a commit message!"
+else
+    echo "Commit: $1"
+    npm run build && gh-pages -d public -b main -m [DEPLOY] $1
+fi
---
+++ src/js/navbar.js
@@ -0,0 +1,37 @@
+
+var nav = $('nav');
+var scrollPosition, navOpacity;
+
+jQuery(function() {
+        scrollPosition = jQuery(window).scrollTop();
+        navOpacity = (scrollPosition / jQuery(window).height()) + 0.2;
+
+        nav.css('background-color', 'rgba(var(--color-primary), ' + navOpacity + ')');
+        jQuery(window).on('scroll', function() {
+                var offset = jQuery(window).scrollTop();
+                var height = nav.height();
+                var heightOffset = 25;
+
+                if(offset >= (height + heightOffset)) {
+                    nav.addClass('sticky');
+                } else if(offset < (height + heightOffset) && nav.hasClass('sticky')) {
+                    nav.removeClass('sticky');
+                }
+
+                console.log(offset + "/" + height);
+
+                scrollPosition = jQuery(window).scrollTop();
+                navOpacity = (scrollPosition / jQuery(window).height()) + 0.2;
+
+                nav.css('background-color', "rgba(var(--color-primary), " + navOpacity + ")");
+        });
+
+        jQuery('nav > ul > li').on('click', function(e) {
+            $('nav > ul > li').removeClass('selected');
+            if($(this).index() == 0) {
+                return;
+            }
+            $(this).addClass('selected');
+            e.preventDefault();
+        });
+});

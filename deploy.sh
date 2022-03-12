19c90828e01565683e7dbb0c617136290fadc3ce
Eduard Wayland <eduproard@gmail.com>
2022-03-12 15:58:23 +0100

    Updated script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ if [[ "$1" =~ ^$ ]]; then
     echo "You must specify a commit message!"
 else
     echo "Commit: $1"
-    npm run build && gh-pages -d public -b main -m [DEPLOY] $1
+    npm run build && gh-pages -d public -b main -m "[DEPLOY] $1"
 fi
